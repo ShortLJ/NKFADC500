34957f87e1a7264ea2f5d7cc9e41450ea1c70240
ShortLJ <47587038+ShortLJ@users.noreply.github.com>
2021-04-30 19:42:18 +0900

    Create TCB_permission.sh

---
+++ TCB_permission.sh
@@ -0,0 +1,2 @@
+echo SUBSYSTEM=="usb",ATTR{idVendor}=="0547",ATTR{idProduct}=="1307",MODE="0666" >> /etc/udev/rules.d/71-notice-dev-permissions.rules
+echo SUBSYSTEM=="usb",ATTR{idVendor}=="0547",ATTR{idProduct}=="1308",MODE="0666" >> /etc/udev/rules.d/71-notice-dev-permissions.rules
