29fbcbf125f6a72340b31d8f66f29c9550bb689f
ShortLJ <47587038+ShortLJ@users.noreply.github.com>
2021-04-30 19:43:50 +0900

    Update TCB_permission.sh

--- TCB_permission.sh
+++ TCB_permission.sh
@@ -1,2 +1,8 @@
+# TCB_permission.sh
+
+#lsusb | grep Anchor ## memo #bus, #Device
+#ll /dev/bus/usb/(#bus)/(#Device)
+#sudo chmod 666 /dev/bus/usb/(#bus)/(#Device)
+
 echo SUBSYSTEM=="usb",ATTR{idVendor}=="0547",ATTR{idProduct}=="1307",MODE="0666" >> /etc/udev/rules.d/71-notice-dev-permissions.rules
 echo SUBSYSTEM=="usb",ATTR{idVendor}=="0547",ATTR{idProduct}=="1308",MODE="0666" >> /etc/udev/rules.d/71-notice-dev-permissions.rules
